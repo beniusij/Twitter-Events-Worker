c01aded8a3e0ad817e82e96f4825382d5fc88183
Juozas Beniusis <beniusij@gmail.com>
2018-02-17 22:44:28 +0000

    Updated twitter config file. Added bearer_token and made client a global variable.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,7 @@
 config = {
-    consumer_key:    "tPTMaqY1wVZyNtAImmGYG9Fvk",
-    consumer_secret: "zjpOZgzivWw3foxLsKDWyiyf65SyK6x3oIImibW2XUq0BXYxQu"
+    consumer_key:     "tPTMaqY1wVZyNtAImmGYG9Fvk",
+    consumer_secret:  "zjpOZgzivWw3foxLsKDWyiyf65SyK6x3oIImibW2XUq0BXYxQu",
+    bearer_token:     "AAAAAAAAAAAAAAAAAAAAADjy4QAAAAAAXzxOXI6s25shOkNTACIm8LvE6Ig%3DPHuXdyHNIS6tFDgyC9gYo50jYYYESlqRImRuHiZXaUnRafGm9O"
 }
 
-client = Twitter::REST::Client.new(config)
+$client = Twitter::REST::Client.new(config)
