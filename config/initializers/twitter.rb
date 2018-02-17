319f3c4c04d9944c0220b74bada5bcc6f9b502e5
Juozas Beniusis <beniusij@gmail.com>
2018-02-17 22:32:47 +0000

    Added config file for twitter

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+config = {
+    consumer_key:    "tPTMaqY1wVZyNtAImmGYG9Fvk",
+    consumer_secret: "zjpOZgzivWw3foxLsKDWyiyf65SyK6x3oIImibW2XUq0BXYxQu"
+}
+
+client = Twitter::REST::Client.new(config)
