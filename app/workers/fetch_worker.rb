16d2f16fdf995c033957f459fa1667250f4e2177
Juozas Beniusis <beniusij@gmail.com>
2018-02-18 23:51:46 +0000

    Added more comments.

--- app/workers/fetch_worker.rb
+++ app/workers/fetch_worker.rb
@@ -5,7 +5,12 @@ class FetchWorker
   # and put it in database.
 
   def perform()
+
+    # Fetch tweets from the platform
     tweets = $client.user_timeline("TheAsylumVenue")
     puts tweets.size
+
+    # FOR EACH tweet IN tweets DO
+    # Store the tweet in PostGIS database
   end
 end
