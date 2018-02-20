6d00d7751ff7c4ac90ab2b7eb466afe5945c0d83
Juozas Beniusis <beniusij@gmail.com>
2018-02-20 11:13:47 +0000

    Wrote save_tweet method in fetch worker and updated perform method to use the new method.

--- app/workers/fetch_worker.rb
+++ app/workers/fetch_worker.rb
@@ -7,10 +7,24 @@ class FetchWorker
   def perform()
 
     # Fetch tweets from the platform
-    tweets = $client.user_timeline("TheAsylumVenue")
+    tweets = $client.user_timeline("TheAsylumVenue", {count: 5})
     puts tweets.size
 
     # FOR EACH tweet IN tweets DO
+    tweets.each do |tweet|
+      save_tweet(tweet)
+    end
     # Store the tweet in PostgreSQL  database
   end
+
+  private
+
+  # Map tweet values and save it to the database
+  def save_tweet(tweet)
+    RawTweet.create(tweet_id: tweet.id) do |t|
+      t.full_text = tweet.full_text
+      t.uri = tweet.uri
+    end
+  end
+
 end
