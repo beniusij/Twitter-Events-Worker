1fc85f860691d06437f9d84bc23e6e4d181c99ac
Juozas Beniusis <beniusij@gmail.com>
2018-02-20 21:26:05 +0000

    Moved main block of code in FetchWorker.perform to a private method. Perform method will iterate through an array of venues and call the new private method.

--- app/workers/fetch_worker.rb
+++ app/workers/fetch_worker.rb
@@ -7,19 +7,11 @@ class FetchWorker
   def perform()
 
     options = {count: 200}
-    # Fetch tweets from the platform
-    tweets = $client.user_timeline("TheAsylumVenue", options)
+    venues = ["TheAsylumVenue"]
 
-    # FOR EACH tweet IN tweets DO
-    tweets.each do |tweet|
-      # Store the tweet in PostgreSQL  database
-      save_tweet(tweet)
+    venues.each do |venue|
+      fetch_job(venue, options)
     end
-
-  # Catch exception
-  rescue Twitter::Error::TooManyRequests => error
-    sleep error.rate_limit.reset_in + 1
-    retry
   end
 
   private
@@ -30,6 +22,22 @@ class FetchWorker
       t.full_text = tweet.full_text
       t.uri = tweet.uri
     end
+  end
+
+  # Fetch tweets from the platform
+  def fetch_job(user, options)
+    tweets = $client.user_timeline("TheAsylumVenue", options)
+
+    # FOR EACH tweet IN tweets DO
+    tweets.each do |tweet|
+      # Store the tweet in PostgreSQL  database
+      save_tweet(tweet)
+    end
+
+      # Catch exception
+  rescue Twitter::Error::TooManyRequests => error
+    sleep error.rate_limit.reset_in + 1
+    retry
   end
 
 end
