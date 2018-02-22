14646724569e76efab10b6479105dda0a3b9fc50
Juozas Beniusis <beniusij@gmail.com>
2018-02-22 12:53:07 +0000

    Wrote a pseudo code for the second worker.

---
+++ app/workers/check_worker.rb
@@ -0,0 +1,17 @@
+class CheckWorker
+  include Sidekiq::Worker
+
+  # Worker will check whether the tweet is valid
+  # by checking if it has words that could be parsed
+  # to a date.
+  def perform()
+    # Get a list of tweets that have not been checked and are not valid
+    # Loop throught the list of tweets
+      # Split the full_text into an array of words full_text.split
+      # Iterate through the array
+        # Strip any non-word characters     word.gsub!(/[^0-9A-Za-z]/, '')
+        # is_valid = Chronic.parse(w).class == Time
+        # if is_valid
+          # Update in db is_valid column by setting it to TRUE
+  end
+end
