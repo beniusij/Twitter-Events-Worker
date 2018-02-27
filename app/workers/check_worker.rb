89bdc115e6fdd34db688a971584569cddfd10e88
Juozas Beniusis <beniusij@gmail.com>
2018-02-27 12:29:51 +0000

    Refactored some of the code in check_worker. Need to find how to validate dd/mm/yyyy date.

--- app/workers/check_worker.rb
+++ app/workers/check_worker.rb
@@ -5,26 +5,15 @@ class CheckWorker
   # Worker will check whether the tweet is valid
   # by checking if it has words that could be parsed
   # to a date.
-  def perform()
+  def perform
     # Get a list of tweets that have not been checked
     options = {
-        is_checked: false
+      is_checked: false
     }
-    # Loop throught the list of tweets
+    # Loop through the list of tweets
     RawTweet.where(options).each do |tweet|
-      is_valid = false
-      # Split the full_text into an array of words full_text.split
-      text = tweet.full_text.split
-      # Iterate through the array
-      text.each do |word|
-        unless is_valid
-          # Strip any non-word characters
-          word = word.gsub(/[^0-9A-Za-z]/, '')
-          is_valid = Chronic.parse(word).class == Time
-        end
-      end
       # Update record, since it has been checked
-      update(tweet, is_valid)
+      update(tweet, valid?(tweet.full_text))
     end
   end
 
@@ -33,10 +22,27 @@ class CheckWorker
   # Update is_valid for an existing record
   def update(tweet, is_valid)
     properties = {
-        is_valid: is_valid,
-        is_checked: true
+      is_valid: is_valid,
+      is_checked: true
     }
     RawTweet.update(tweet.id, properties)
+  end
+
+  # Evaluate if the message contains verbal date
+  def valid?(text)
+    is_valid = false
+    # Split the full_text into an array of words full_text.split
+    text = text.split
+    # Iterate through the array
+    text.each do |word|
+      unless is_valid
+        # Strip any non-word characters
+        word = word.gsub(/[^0-9A-Za-z]/, '')
+        is_valid = Chronic.parse(word).class == Time
+
+      end
+    end
+    is_valid
   end
 
 end
