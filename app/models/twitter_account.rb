a729f0e8a8d9ca63c0ad84839abe0406941d83dc
Juozas Beniusis <beniusij@gmail.com>
2018-04-17 00:58:43 +0100

    Added a method for adding accounts.

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -5,4 +5,11 @@ class TwitterAccount < ApplicationRecord
   def self.least_checked(num)
     TwitterAccount.order(last_checked: :asc).limit(num)
   end
+
+  def self.add_account(username)
+    TwitterAccount.create do |t|
+      t.twitter_name = username
+      t.last_checked = Time.now - 1.day
+    end
+  end
 end
